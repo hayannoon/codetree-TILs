6c67ad6a9196e2c87deec3199c9da3b255de45e1
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2024-05-30 23:49:03 +0900

    Title: 행복한 수열의 개수, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240530/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240530/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,35 +1,27 @@
-def get_max_continue(li):
-    max_val = 1
-    cur_val = 1
-    for i in range(1, len(li)):
-        if li[i] == li[i-1]:
-            cur_val += 1
-            max_val = max(max_val, cur_val)
+def get_max_continue(sequence):
+    max_length = 1
+    current_length = 1
+    for i in range(1, len(sequence)):
+        if sequence[i] == sequence[i-1]:
+            current_length += 1
+            max_length = max(max_length, current_length)
         else:
-            cur_val = 1
-    return max_val
+            current_length = 1
+    return max_length
             
 if __name__ == "__main__":
-    n, m = map(int, input().split())
+    rows, min_sequence_length = map(int, input().split())
 
-    li = []
-    for _ in range(n):
-        li.append(list(map(int,input().split())))
+    matrix = [list(map(int,input().split())) for _ in range(rows)]
         
-    cnt = 0
+    count = 0
 
-    for i in range(n):
-        tmp = []
-        for j in range(n):
-            tmp.append(li[i][j])
-        if get_max_continue(tmp) >= m:
-            cnt+=1
-            
-    for i in range(n):
-        tmp = []
-        for j in range(n):
-            tmp.append(li[j][i])
-        if get_max_continue(tmp) >= m:
-            cnt += 1
+    for row in matrix:
+        if get_max_continue(row) >= min_sequence_length:
+            count += 1
+
+    for column in zip(*matrix):
+        if get_max_continue(column) >= min_sequence_length:
+            count += 1
     
-    print(cnt)
+    print(count)
