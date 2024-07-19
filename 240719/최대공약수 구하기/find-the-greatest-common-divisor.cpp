b413cafdea5182e032e64b0db1817a7a16dd4301
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2024-07-19 22:50:50 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240719/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int solution(int n, int m) {
+int solution(int n, int m)
+{
 
-    int bigNumber = (n>m) ? n : m;
-    int smallNumber = (n<m) ? n : m;
+    int bigNumber = (n > m) ? n : m;
+    int smallNumber = (n < m) ? n : m;
     int answer = 1;
+    int i = 2;
 
-    for(int i = 2 ; i <= smallNumber ; i++) {
-        if(n % i == 0 && m % i == 0) {
+    for (i; i <= smallNumber; i++)
+    {
+        if (n % i == 0 && m % i == 0)
+        {
             n /= i;
             m /= i;
             answer *= i;
+            i = 1;
+        }
+        if (n == 1 || m == 1)
+        {
+            break;
         }
     }
 
     return answer;
 }
 
-
-int main() {
+int main()
+{
     int n, m;
-    cin >> n >> m ;
+    cin >> n >> m;
+
     cout << solution(n, m) << endl;
 
     return 0;
